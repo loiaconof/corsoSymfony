76e2c1e31d6c29efd5544361d9c6141600682e24
loiaconof <92976965+loiaconof@users.noreply.github.com>
2022-12-18 19:40:24 +0100

    initial commit

---
+++ src/Controller/HelloController.php
@@ -0,0 +1,38 @@
+<?php
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HelloController extends AbstractController
+{
+    private array $messages = [
+        ['message' => 'Hi!', 'created' => '2022/11/28'],
+        ['message' => 'Hello!', 'created' => '2022/11/28'],
+        ['message' => 'Bye!', 'created' => '2022/11/28']
+    ];
+
+    #[Route('/{limit?3}', name: 'app_index')]
+    public function index(int $limit): Response
+    {
+        return $this->render(
+            'hello/index.html.twig',
+            [
+                'messages' => $this->messages,
+                'limit'    => $limit
+            ]
+        );
+    }
+
+    #[Route('/message/{id<\d+>}', name: 'app_show_one')]
+    public function showOne(int $id): Response
+    {
+        return $this->render(
+            'hello/show_one.html.twig',
+            [
+                'message' => $this->messages[$id]
+            ]
+        );
+    }
+}
