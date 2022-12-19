26e8592c12d0d6d26a9eed911dd0da5c184bb39f
loiaconof <92976965+loiaconof@users.noreply.github.com>
2022-12-19 07:29:32 +0100

    38 Form Classes

--- src/Controller/MicroPostController.php
+++ src/Controller/MicroPostController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\MicroPost;
+use App\Form\MicroPostType;
 use App\Repository\MicroPostRepository;
 use DateTime;
 use Symfony\Component\HttpFoundation\Request;
@@ -32,11 +33,7 @@ class MicroPostController extends AbstractController
     #[Route('/micro/post/add', name: 'app_micro_post_add', priority: 2)]
     public function add(Request $request, MicroPostRepository $posts): Response
     {
-        $microPost = new MicroPost();
-        $form = $this->createFormBuilder($microPost)
-            ->add('title')
-            ->add('text')
-            ->getForm();
+        $form = $this->createForm(MicroPostType::class, new MicroPost());
 
         $form->handleRequest($request);
 
@@ -60,10 +57,7 @@ class MicroPostController extends AbstractController
     #[Route('/micro/post/{post}/edit', name: 'app_micro_post_edit')]
     public function edit(MicroPost $post, Request $request, MicroPostRepository $posts): Response
     {
-        $form = $this->createFormBuilder($post)
-            ->add('title')
-            ->add('text')
-            ->getForm();
+        $form = $this->createForm(MicroPostType::class, $post);
 
         $form->handleRequest($request);
 
